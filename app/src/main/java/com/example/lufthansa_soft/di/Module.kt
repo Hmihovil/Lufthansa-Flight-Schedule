4cdb2f2f218b61b1a5f78eb772eb12359b855fff
pawnjester <ebukeokonji@gmail.com>
2020-03-22 01:06:54 -0700

    first commit

---
+++ app/src/main/java/com/example/lufthansa_soft/di/Module.kt
@@ -0,0 +1,2 @@
+package com.example.lufthansa_soft.di
+
---
+++ app/src/main/java/com/example/lufthansa_soft/ui/AirlineScheduleAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.lufthansa_soft.ui
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lufthansa_soft.databinding.AirlineScheduleItemBinding
+
+class AirlineScheduleAdapter: RecyclerView.Adapter<AirlineScheduleAdapter.Viewholder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: Viewholder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class Viewholder(val binding: AirlineScheduleItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
